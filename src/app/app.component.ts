63e797692096b8db37b02c828ac28ebd3a83a17a
avondo yann <yann.avondo@gmail.com>
2017-11-02 20:55:31 +0100

    :sparkles: table of amount

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
-
+import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
+import { HttpParams } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -9,18 +10,64 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+ 
+
+  constructor(private http: HttpClient) {
+  }
   title = 'app';
   // Pie
   public pieChartLabels: string[] = ['Revenus', 'Dépenses'];
   public pieChartData: number[] = [300, 500];
   public pieChartType: string = 'pie';
+  public connectValues: any;
+  public transferValue: any;
 
+  public connect(ngForm) {
+    console.log(ngForm);
+    this.http.put("https://bankin-ingesup.herokuapp.com/auth/login", ngForm).subscribe(data => {
+      this.connectValues = data
+    }
+    );
+    this.getUserTransfer();
+  }
+
+  public outMoney(ngForm) {
+    this.http.get("https://bankin-ingesup.herokuapp.com/transfer/" + this.connectValues.user.username).subscribe(data =>
+      console.log(data)
+    );
+  }
+
+  public getUserTransfer() {
+    
+    this.http.get("https://bankin-ingesup.herokuapp.com/transfer/" + this.connectValues.user.username,
+      {
+        headers: new HttpHeaders().set('Authorization', this.connectValues.token)
+      }
+    ).subscribe(data => {
+      this.transferValue = data;
+    });
+    this.transferValue.transfers.forEach(element => {
+      console.log(element);
+    });
+  }
   // events
   public chartClicked(e: any): void {
     console.log(e);
   }
-
   public chartHovered(e: any): void {
     console.log(e);
   }
+  public ngShowForm(connectValues)
+  {
+      if (connectValues == null)
+      {
+      return false;
+      }
+      else
+      {
+      return true;
+      }
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ChartsModule } from 'ng2-charts';
+
+import {HttpClientModule} from '@angular/common/http';
 
 // In your App's module:
 @NgModule({
@@ -11,9 +14,12 @@ import { ChartsModule } from 'ng2-charts';
   ],
   imports: [
     BrowserModule,
-    ChartsModule 
+    ChartsModule,
+    HttpClientModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
